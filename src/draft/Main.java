59077da356410ec99691708fb71fdf8a64e54c27
Eugeniy <zhenya.0295@mail.ru>
2023-03-27 23:55:31 +0300

    Added LinkedList

---
+++ src/draft/Main.java
@@ -0,0 +1,21 @@
+package draft;
+
+
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        MyList myList = new MyList();
+        myList.add(0);
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(5);
+        myList.add(4, 4);
+        //myList.removingRepetitions(5);
+        //myList.remove(3);
+        myList.remove(value -> value == 5);
+        System.out.println(myList);
+
+    }
+}
---
+++ src/draft/MyList.java
@@ -0,0 +1,124 @@
+package draft;
+
+import java.util.function.Predicate;
+
+public class MyList {
+
+    private Node head;
+
+    public MyList() {
+        head = null;
+    }
+
+    public static class Node {
+        public int data;
+        public Node next;
+
+        public Node (int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public void add(int value) {
+        Node newNode = new Node(value);
+        Node correntNode = head;
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (correntNode.next != null) {
+                correntNode = correntNode.next;
+            }
+            correntNode.next = newNode;
+        }
+    }
+
+    public void add(int index, int value) {
+        Node newNode = new Node(value);
+        Node correntNode = head;
+        Node lastNode = null;
+        if (index > counting() && (index >= 0)) {
+            add(value);
+        }else {
+            if (head == null) {
+                head = new Node(value);
+            } else {
+                for (int i = 0; i < index; i++) {
+                    if (correntNode.next == null) {
+                        correntNode.next = new Node(value);
+                        break;
+                    }
+                    lastNode = correntNode;
+                    correntNode = correntNode.next;
+                }
+                newNode.next = correntNode;
+                lastNode.next = newNode;
+            }
+        }
+    }
+    public void remove(int index) {
+        Node correntNode = head;
+        Node lastNode = null;
+        if (index > counting() && (index >=0)) {
+            System.out.println("Ошибка введенного индекса");
+        } else {
+            for (int i = 0; i < index; i++) {
+                lastNode = correntNode;
+                correntNode = correntNode.next;
+            }
+            lastNode.next = correntNode.next;
+            }
+    }
+
+    public void remove(Predicate<Integer> predicate) {
+        Node correntNode = head;
+        Node lastNode = null;
+
+        while (correntNode.next != null) {
+
+            if (predicate.test(correntNode.data)) { // TODO: 27/03/2023 implement with predicate
+                if (correntNode == head) {
+                    head = correntNode.next;
+                } else {
+                    lastNode.next = correntNode.next;
+                }
+            }
+            lastNode = correntNode;
+            correntNode = correntNode.next;
+        }
+    }
+
+    private int counting() {
+        int check = 0;
+        Node currentNode = head;
+        if (head == null) {
+            return check;
+        } else {
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+                check++;
+            }
+            return check;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        Node currentNode = head;
+        if (head == null) {
+            //System.out.println(head.data);
+            result = String.valueOf(head.data);
+            return result;
+        }
+        while (currentNode.next != null) {
+            result += currentNode.data + "\n";
+            currentNode = currentNode.next;
+            //System.out.println(currentNode.data);
+            //result += currentNode.data;
+        }
+        result += currentNode.data;
+        return result;
+    }
+}
